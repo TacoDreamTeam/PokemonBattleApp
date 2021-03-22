6abae48e33bcf1b3d05b89a9dfac53d521e1ca79
Omar Sancho <omarsancho08@gmail.com>
2021-03-22 12:45:27 -0400

    testing

---
+++ PokemonBattleApp/src/test/java/com/revature/test/Test.java
@@ -0,0 +1,14 @@
+package com.revature.test;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+		
+		System.out.println("sdjknbfjasklbd");
+	}
+
+}
