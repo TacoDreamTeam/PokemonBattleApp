45a0bf4738c0710d0bf9ecd8d232d747d0fa7a23
neco401 <jacl212010@hotmail.com>
2021-03-25 15:28:44 -0400

    added register method to Service class

--- PokemonBattleApp/src/main/java/com/revature/service/Service.java
+++ PokemonBattleApp/src/main/java/com/revature/service/Service.java
@@ -3,19 +3,22 @@ package com.revature.service;
 import org.apache.log4j.Logger;
 
 import com.revature.models.Trainer;
-import com.revature.repo.TrainerDao;
 
 public class Service {
+	
+	private static Logger log = Logger.getLogger(Service.class);
+	private static TrainerService trainerService = new TrainerServiceImpl();
 
 	public static Trainer login(String trainerUsername, String password) {
-		TrainerDao tDao=new TrainerDao();
 		
-		Trainer trainer=tDao.selectTrainerByUsername(trainerUsername);
-		if (trainer!=null && trainer.getPassword().equals(password)){
-			return trainer;
-		}else {
-			return null;
-		}	
+		Trainer t = trainerService.login(trainerUsername, password);
+		log.info("Logining in Trainer username: " + t.getUsername());
+		return t;
+	}
+	
+	public static boolean registerTrainer(Trainer t) {
+		trainerService.insertTrainer(t);
+		return t.getTrainerId() != 0;
 	}
 	
 }
