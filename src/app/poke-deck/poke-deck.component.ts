fa207299f21bc6c012c68bc5c2fdd637d2904cb4
neco401 <jacl212010@hotmail.com>
2021-03-26 19:44:57 -0400

    fleshed out PokeDeckComponent

--- src/app/poke-deck/poke-deck.component.ts
+++ src/app/poke-deck/poke-deck.component.ts
@@ -1,3 +1,5 @@
+import { PokeDeckService } from './../services/poke-deck.service';
+import { PokeDeck } from './../models/pokeDeck.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeDeckComponent implements OnInit {
 
-  constructor() { }
+  public pokeDeck: PokeDeck[] = [];
+
+  constructor(private pokeDeckService: PokeDeckService) { }
 
   ngOnInit(): void {
+    this.findPokeDeck();
   }
 
+  public findPokeDeck(){
+    this.pokeDeckService.findAllPokeDeck().subscribe(data => this.pokeDeck = data)
+  }
 }
