3b3c879a7fb7ff6ae9ed90a0b84303e2cbd31e68
ammevissen <mevissenjobs@gmail.com>
2021-03-30 17:16:56 -0500

    updated pokeDex model

--- src/app/models/pokeDex.ts
+++ src/app/models/pokeDex.ts
@@ -1,8 +1,10 @@
 export class pokeDex{
+    "id":number;
     "trainerId": number;
     "pokemonId": number;
 
-    constructor(trainerId: number, pokemonId: number){
+    constructor(id: number, trainerId: number, pokemonId: number){
+        this.id=id;
         this.trainerId=trainerId;
         this.pokemonId=pokemonId;
     }
